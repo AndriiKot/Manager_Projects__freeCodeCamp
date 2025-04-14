9b42407f4e8cbd541f21c7996874e90c843b545e
AndriiKot <AndriiKot@users.noreply.github.com>
2025-04-14 15:46:23 +0200

    'Manager projects: freeCodeCamp' ! **init: 'ManagerBuildingMagazine'

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
 const README_CONFIG = {
-  README_TOPIC: "\n# Building a Calorie Counter\n",
-  TECHNOLOGIES: ["JavaScript", "CSS", "HTML5"],
+  README_TOPIC: "\n# Building a Magazine\n",
+  TECHNOLOGIES: ["CSS", "HTML5"],
   LINKS_TO_DOCS_TECHNOLOGIES: "technologies/docs_links.json",
-  FILES: ["index.html", "styles.css", "script.js"],
+  FILES: ["index.html", "styles.css"],
   top_page: "\n<a id=top></a>\n",
   back_to_top_page: "\n\n[back to top](#top)\n\n",
   PREVIEW: "../images/previews/",
   BASE_URL:
-    "https://github.com/AndriiKot/JS__Validation_by_Building_a_Calorie_Counter__freeCodeCamp/",
+    "https://github.com/AndriiKot/Desing__CSS_Grid_by_Building_a_Magazine__freeCodeCamp/",
   BRANCH: "main",
   BASE_URL_TECHNOLOGIES:
     "https://github.com/AndriiKot/iconsSVG_and_linksDocs/blob/main/svg/",
-  BASE_URL_PROJECT: "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/learn-form-validation-by-building-a-calorie-counter/",
+  BASE_URL_PROJECT: "https://www.freecodecamp.org/learn/2022/responsive-web-design/#learn-css-grid-by-building-a-magazine/",
 };
 
 module.exports = README_CONFIG;
+
