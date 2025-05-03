42eba0f5ae544b401941e40cf4cb729c3f4c7579
AndriiKot <AndriiKot@users.noreply.github.com>
2025-05-03 18:43:31 +0200

    'Manager projects: freeCodeCamp' ! **init new 'ManagerCitySkyline'

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const README_CONFIG = {
-  README_TOPIC: "\n# Cat Photo App\n",
-  TECHNOLOGIES: ["HTML5"],
+  README_TOPIC: "\n# City Skyline\n",
+  TECHNOLOGIES: ["HTML5", "CSS"],
   LINKS_TO_DOCS_TECHNOLOGIES: "technologies/docs_links.json",
-  FILES: ["index.html"],
+  FILES: ["index.html", "styles.css"],
   top_page: "\n<a id=top></a>\n",
   back_to_top_page: "\n\n[back to top](#top)\n\n",
   PREVIEW: "../images/previews/",
   BASE_URL:
-    "https://github.com/AndriiKot/Desing___Cat_Photo_App___freeCodeCamp/",
+    "https://github.com/AndriiKot/Desing___City_Skyline___freeCodeCamp/",
   BRANCH: "main",
   BASE_URL_TECHNOLOGIES:
     "https://github.com/AndriiKot/iconsSVG_and_linksDocs/blob/main/svg/",
-  BASE_URL_PROJECT: "https://www.freecodecamp.org/learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/",
+  BASE_URL_PROJECT: "https://www.freecodecamp.org/learn/2022/responsive-web-design/learn-css-variables-by-building-a-city-skyline/",
 };
 
 module.exports = README_CONFIG;
--- manager_readmes.js
+++ manager_readmes.js
@@ -177,7 +177,7 @@ const README_MAIN = [
   topic,
   generateDetailsTemplate("Follow Links Steps", table),
   generateImagePreview(base_url, 4, getFiles(folderImagesPreviews).at(-1)),
-//  back_to_top,
+  back_to_top,
   generateTableTechnologies(config.TECHNOLOGIES, 33, 100, 100, 100),
   back_to_top,
   generateCodesProject(),
