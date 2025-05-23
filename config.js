ce97efab56eaf6bf02f5dd7260b221c89b5737cb
AndriiKot <AndriiKot@users.noreply.github.com>
2025-05-23 15:29:39 +0200

    'Manager projects: freeCodeCamp' !  **init: 'ManagerAppliedVisualDesign'  !

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const README_CONFIG = {
-  README_TOPIC: "\n# Building a Calorie Counter\n",
-  TECHNOLOGIES: ["JavaScript", "CSS", "HTML5"],
+  README_TOPIC: "\n# Applied Visual Design\n",
+  TECHNOLOGIES: ["CSS", "HTML5"],
   LINKS_TO_DOCS_TECHNOLOGIES: "technologies/docs_links.json",
-  FILES: ["index.html", "styles.css", "script.js"],
+  FILES: ["index.html", "styles.css"],
   top_page: "\n<a id=top></a>\n",
   back_to_top_page: "\n\n[back to top](#top)\n\n",
   PREVIEW: "../images/previews/",
   BASE_URL:
-    "https://github.com/AndriiKot/JS__Validation_by_Building_a_Calorie_Counter__freeCodeCamp/",
+    "https://github.com/AndriiKot/Desing___Applied_Visual_Design___freeCodeCamp/",
   BRANCH: "main",
   BASE_URL_TECHNOLOGIES:
     "https://github.com/AndriiKot/iconsSVG_and_linksDocs/blob/main/svg/",
-  BASE_URL_PROJECT: "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/learn-form-validation-by-building-a-calorie-counter/",
+  BASE_URL_PROJECT: "https://www.freecodecamp.org/learn/responsive-web-design/applied-visual-design/2",
 };
 
 module.exports = README_CONFIG;
